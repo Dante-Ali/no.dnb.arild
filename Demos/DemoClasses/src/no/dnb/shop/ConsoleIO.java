50fb9778a2a85d86d529c97af59a890e673a44b7
Andy Olsen <andyo@olsensoft.com>
2021-01-28 17:49:32 +0000

    Update ConsoleIO.java

--- Demos/DemoClasses/src/no/dnb/shop/ConsoleIO.java
+++ Demos/DemoClasses/src/no/dnb/shop/ConsoleIO.java
@@ -18,7 +18,9 @@ public class ConsoleIO {
         return scanner.nextLine();
     }
 
-
+    // ANDY: You don't want these get/set methods anyway - you don't want to expose the Scanner to the outside world, it's just for internal usage inside this class.
+    //       If you really did want to implement these methods, they should to be static.
+    
     /* Not much fun getting intelliJ to make getters and setters of this class .
     public Scanner getScanner() {
         return scanner;
